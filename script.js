efa50a127f44a4a0997fba8519f72fa5ef8e6e2a
ArchNineTails <timothyhimorbasmayor@gmail.com>
2026-08-13 22:06:04 +0800

    .

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-if (submittedAnswer.toLowerCase().trim() === "example") {
-    window.location.href = "https://www.youtube.com/watch?v=FtutLA63Cp8";
-} else {
-    alert("Authentication failed.");
-}
+document.getElementById("authForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const submittedAnswer = document.getElementById("answer").value;
+
+    if (submittedAnswer.toLowerCase().trim() === "example") {
+        window.location.href = "https://www.youtube.com/watch?v=FtutLA63Cp8;
+    } else {
+        alert("Authentication failed.");
+    }
+});
 
 /* If you see this, you deserve to be punished in hell for your ignorance. Count your days, fool. */
 
